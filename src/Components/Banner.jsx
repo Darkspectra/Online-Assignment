0006cc57b22e2a605d6a198c42e93cc76ed8a9cd
Darkspectra <ftanvirnovo@gmail.com>
2023-11-05 04:05:47 +0600

    created banner outlet

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,11 +1,14 @@
-// import bg1 from "../assets/banner.jpg"
+import bg1 from "../assets/logo.jpg"
 
 const Banner = () => {
     return (
-        <div>
-            
-            <img className="rounded-2xl" src="" alt="" />
-            <p className="text-6xl font-bold my-7">Coming soon..</p>
+        <div className="flex max-w-7xl mx-auto items-center border-8">
+            <div>
+                <img className="rounded-2xl" src={bg1} alt="" />
+            </div>
+            <div >
+                <p className="ml-24 text-6xl font-bold my-7">An Efficient way <br /> to check Assignment</p>
+            </div>
         </div>
     );
 };
